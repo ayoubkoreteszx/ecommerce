248ed762565d7cc7db54dc2066906b5e32f9b7fa
ayoubkoreteszx <65472358+ayoubkoreteszx@users.noreply.github.com>
2021-08-12 20:55:10 +0100

    Update settings.py

--- ecommerce2/ecommerce/settings.py
+++ ecommerce2/ecommerce/settings.py
@@ -142,9 +142,9 @@ META_DESCRIPTION = 'Un bon tailleur de costume et un professionnel qui possède 
 #EMAIL SETTINGS
 EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='idrissiashop@gmail.com'
-EMAIL_PORT=587
-EMAIL_HOST_PASSWORD='gcrprlgwsbqygzba'
+EMAIL_HOST_USER='ur_mail'
+EMAIL_PORT=PORT_NUMBER
+EMAIL_HOST_PASSWORD='ur_password'
 EMAIL_USE_TLS=True
 EMAIL_USE_SSL=False
 
